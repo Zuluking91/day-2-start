27eb637b946302a468f7bd48251d52c096937eb9
Zuluking91 <52240454+Zuluking91@users.noreply.github.com>
2022-09-15 10:06:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+# #Data Types
+
+# #string
+
+# print("Hello" [4])
+
+# #Integer (whole numbers, positive or negative)
+
+# print(123 + 345)
+
+# print(123_456_789)
+
+# #Float (floating point number)
+
+# print(3.14159)
+
+# #Boolean (True or False)
+
+# True
+# False
+
+# num_char = len(input("What is your name?"))
+
+# # str function
+# new_num_char = str(num_char)
+
+# # print("Your name has " + new_num_char + "characters.")
+# print("|Your name has " + new_num_char + " characters.")
+
+# # type check function (controleer de datatype van een variabele)
+# print(type(num_char))
+
+a = float(123)
+print(type(a))
+
+# print(70 + float("100.5"))
+print(str(70) + str(100))
